2c34c02cfe6f60978a9a0acda82f1fe56edaa830
MurugalakshmiM <mmurugalakshmi.03@gmail.com>
2020-08-03 10:29:47 +0530

    dashboard 6.7 and 9 mockdata table creation

--- src/modules/dashboard/services/dashboard.service.ts
+++ src/modules/dashboard/services/dashboard.service.ts
@@ -1,11 +1,40 @@
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Injectable } from '@angular/core'
+import { throwError as observableThrowError, Observable, of } from 'rxjs'
+import { catchError, map } from 'rxjs/operators'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import get from 'lodash/get';
 
-@Injectable()
+
+
+@Injectable({
+    providedIn: 'root'
+})
 export class DashboardService {
-    constructor() {}
+  private http: HttpClient
+  constructor (http: HttpClient) {
+    this.http = http
+  }
 
-    getDashboard$(): Observable<{}> {
-        return of({});
-    }
+  getDashboard$ (): Observable<{}> {
+    return of({})
+  }
+
+  getContentWiseList (): Observable<any> {
+    return this.httpGet('http://168.61.44.82:8080/api/v1/min_fixlets/0/25/0/0').pipe(
+        map(res => res),
+        catchError(this.handleError)
+      );
+  }
+
+  httpGet(path: string, module = '0'): Observable<any> {
+    return this.http.get( path)
+      .pipe(
+        map(res => res),
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(error: Response): any {
+    return observableThrowError(get(error, 'error') || {});
+  }
 }
